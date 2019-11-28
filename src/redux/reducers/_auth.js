56671a6418092ecdc80c02011dfca72ac7a4db5a
Joey Zhou <jfzhou.nc@gmail.com>
2019-11-27 23:45:16 -0500

    fix editor height

--- src/redux/reducers/_auth.js
+++ src/redux/reducers/_auth.js
@@ -5,20 +5,18 @@ export default function authReducer(state = initialState.auth, action) {
   switch (action.type) {
     case types.CONNECTING_TO_AWS:
       return { ...state, isConnecting: true };
+
     case types.CONNECTED_TO_AWS:
-      const payload = {
+      return {
+        ...state,
+        isConnecting: false,
+        isAuthenticated: true,
         user: action.payload.authUser,
         userpoolId: action.payload.connectionDetails.userpoolId,
         userpoolClientId: action.payload.connectionDetails.userpoolClientId,
         graphqlEndpoint: action.payload.connectionDetails.graphqlEndpoint,
       };
-      return {
-        ...state,
-        ...payload,
-        isConnecting: false,
-        isAuthenticated: true,
-        user: action.payload,
-      };
+
     default:
       return state;
   }
