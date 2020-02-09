217f090155bf5958c07a9552493ba47f8ff2b3e8
Joey Zhou <jfzhou.nc@gmail.com>
2020-02-08 23:25:07 -0500

    add redux-logger (#2)

--- src/store/index.js
+++ src/store/index.js
@@ -2,9 +2,17 @@ import thunk from 'redux-thunk';
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import authReducer from './slices/auth';
 
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV === `development`) {
+  const { logger } = require(`redux-logger`);
+
+  middlewares.push(logger);
+}
+
 export default configureStore({
   reducer: {
     auth: authReducer,
   },
-  middleware: [...getDefaultMiddleware(), thunk],
+  middleware: [...getDefaultMiddleware(), ...middlewares],
 });
