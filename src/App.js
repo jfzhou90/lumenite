043326df480e477ec54fb7b51ef9ba8943040ccb
Joey Zhou <jfzhou.nc@gmail.com>
2020-02-11 23:14:23 -0500

    move toast to indexjs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { ToastContainer } from 'react-toastify';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import { ProtectedRoute } from './lib/router/routes';
@@ -7,22 +6,17 @@ import { ProtectedRoute } from './lib/router/routes';
 import LoginPage from './modules/login';
 import GraphQLEditor from './modules/graphiqlEditor';
 
-import 'react-toastify/dist/ReactToastify.css';
-
 const App = () => (
-  <>
-    <ToastContainer autoClose={3000} hideProgressBar />
-    <BrowserRouter>
-      <Switch>
-        <Route exact path='/login'>
-          <LoginPage />
-        </Route>
-        <ProtectedRoute path='/'>
-          <GraphQLEditor />
-        </ProtectedRoute>
-      </Switch>
-    </BrowserRouter>
-  </>
+  <BrowserRouter>
+    <Switch>
+      <Route exact path='/login'>
+        <LoginPage />
+      </Route>
+      <ProtectedRoute path='/'>
+        <GraphQLEditor />
+      </ProtectedRoute>
+    </Switch>
+  </BrowserRouter>
 );
 
 export default App;
