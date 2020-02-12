a9ccab774cfe17e41a90134645d8e284acffab8f
Joey Zhou <jfzhou.nc@gmail.com>
2020-02-11 23:11:53 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import { ProtectedRoute } from './lib/router/routes';
 
 import LoginPage from './modules/login';
 import GraphQLEditor from './modules/graphiqlEditor';
 
 import 'react-toastify/dist/ReactToastify.css';
 
-const App = () => {
-  const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
-
-  return (
-    <>
-      <ToastContainer autoClose={3000} hideProgressBar />
-      {isAuthenticated ? <GraphQLEditor /> : <LoginPage />}
-    </>
-  );
-};
+const App = () => (
+  <>
+    <ToastContainer autoClose={3000} hideProgressBar />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/login'>
+          <LoginPage />
+        </Route>
+        <ProtectedRoute path='/'>
+          <GraphQLEditor />
+        </ProtectedRoute>
+      </Switch>
+    </BrowserRouter>
+  </>
+);
 
 export default App;
